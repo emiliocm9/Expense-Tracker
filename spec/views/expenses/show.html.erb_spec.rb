6094f75c0d9afae81fb26ff865ce50f254d480c2
emiliocm9 <emilio.contreras97@gmail.com>
2020-08-28 17:16:26 -0500

    refactor: sickler corrections

--- spec/views/expenses/show.html.erb_spec.rb
+++ spec/views/expenses/show.html.erb_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "expenses/show", type: :view do
+RSpec.describe 'expenses/show', type: :view do
   before(:each) do
     @expense = assign(:expense, Expense.create!(
-      user: nil,
-      group: nil,
-      name: "Name",
-      amount: "9.99"
-    ))
+                                  user: nil,
+                                  group: nil,
+                                  name: 'Name',
+                                  amount: '9.99'
+                                ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(//)
     expect(rendered).to match(//)
--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -1,24 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "users/edit", type: :view do
+RSpec.describe 'users/edit', type: :view do
   before(:each) do
     @user = assign(:user, User.create!(
-      name: "MyString",
-      username: "MyString",
-      password_digest: "MyString"
-    ))
+                            name: 'MyString',
+                            username: 'MyString',
+                            password_digest: 'MyString'
+                          ))
   end
 
-  it "renders the edit user form" do
+  it 'renders the edit user form' do
     render
 
-    assert_select "form[action=?][method=?]", user_path(@user), "post" do
+    assert_select 'form[action=?][method=?]', user_path(@user), 'post' do
+      assert_select 'input[name=?]', 'user[name]'
 
-      assert_select "input[name=?]", "user[name]"
+      assert_select 'input[name=?]', 'user[username]'
 
-      assert_select "input[name=?]", "user[username]"
-
-      assert_select "input[name=?]", "user[password_digest]"
+      assert_select 'input[name=?]', 'user[password_digest]'
     end
   end
 end
