070728071899e077c77a9ad108b1d134981328a8
emiliocm9 <emilio.contreras97@gmail.com>
2020-08-28 18:32:11 -0500

    docs: sessions controller with logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :sessions
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
   resources :expenses
   resources :groups
   resources :users
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
