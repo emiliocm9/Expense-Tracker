4d7176d7f3da164b096b92e0f899f5f00f850fad
emiliocm9 <emilio.contreras97@gmail.com>
2020-08-28 18:54:56 -0500

    refactor: sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    @user = User.find_by(username: params[:username])
-    if @user & @user.authenticate(params[:password])
+    @user = User.find_by(name: params[:name])
+    if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user), notice: 'Logged In'
     else
