0d5c4a364d471e6dca43f050b843a52a6b6d9764
emiliocm9 <emilio.contreras97@gmail.com>
2020-09-07 11:51:40 -0500

    docs: expenses helper method for image

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,17 @@
 module ExpensesHelper
+  def expense_icon(expense)
+    if expense.group.icon.attached?
+      image_tag url_for(expense.group.icon), class: 'w-80'
+    else
+      image_tag 'https://bulma.io/images/placeholders/128x128.png', class: 'w-80'
+    end
+  end
+
+  def expense_group(expense)
+    if expense.group
+      expense_icon(expense)
+    else
+      image_tag 'https://bulma.io/images/placeholders/128x128.png', class: 'w-80'
+    end
+  end
 end
