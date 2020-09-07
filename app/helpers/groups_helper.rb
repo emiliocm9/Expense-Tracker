03468671ccf5d4038077260ff8699052f4127d14
emiliocm9 <emilio.contreras97@gmail.com>
2020-09-07 12:08:01 -0500

    docs: groups edit helper methods

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,9 @@
 module GroupsHelper
+  def groups_form(group)
+    if group.icon.attached?
+      ('<div class="w-100 text-center">' + (image_tag url_for(group.icon), class: 'header_avatar-image mx-auto') + '</div>').html_safe
+    else
+      '<div class="w-100 text-center"><span>This group does not have an icon yet.</span></div>'.html_safe
+    end
+  end
 end
