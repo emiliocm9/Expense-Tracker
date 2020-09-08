7f725857016a91d7b71fd354705daf5ce7c598a0
emiliocm9 <emilio.contreras97@gmail.com>
2020-09-07 19:50:55 -0500

    docs: notices helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def notice_helper
+    return unless notice.present?
+
+    ('<div class="w-100 text-center text-uppercase"><p id="notice">' + notice + '</p></div>').html_safe
+  end
 end
