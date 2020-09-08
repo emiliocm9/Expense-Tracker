0ba63d78a4b142b2dc03933753467c1361583c0c
emiliocm9 <emilio.contreras97@gmail.com>
2020-09-08 13:14:40 -0500

    refactor: monthly expenses heroku

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -6,96 +6,84 @@ class MonthsController < ApplicationController
   end
 
   def jan
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '01')
-    @august = Expense.where('extract(month from created_at) = ?', '01')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '01')
     @total = @august.pluck(:amount).sum
     average_a
     pack
   end
 
   def feb
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '02')
-    @august = Expense.where('extract(month from created_at) = ?', '02')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '02')
     @total = @august.pluck(:amount).sum
     average_feb
     pack
   end
 
   def march
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '03')
-    @august = Expense.where('extract(month from created_at) = ?', '03')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '03')
     @total = @august.pluck(:amount).sum
     average_a
     pack
   end
 
   def april
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '04')
-    @august = Expense.where('extract(month from created_at) = ?', '04')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '04')
     @total = @august.pluck(:amount).sum
     average_b
     pack
   end
 
   def may
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '05')
-    @august = Expense.where('extract(month from created_at) = ?', '05')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '05')
     @total = @august.pluck(:amount).sum
     average_a
     pack
   end
 
   def june
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '06')
-    @august = Expense.where('extract(month from created_at) = ?', '06')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '06')
     @total = @august.pluck(:amount).sum
     average_b
     pack
   end
 
   def july
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '07')
-    @august = Expense.where('extract(month from created_at) = ?', '07')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '07')
     @total = @august.pluck(:amount).sum
     average_a
     pack
   end
 
   def aug
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '08')
-    @august = Expense.where('extract(month from created_at) = ?', '08')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '08')
     @total = @august.pluck(:amount).sum
     average_a
     pack
   end
 
   def sept
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '09')
-    @august = current_user.expenses.where('extract(month from created_at) = ?', '09')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '09')
     @total = @august.pluck(:amount).sum
     average_b
     pack
   end
 
   def oct
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '10')
-    @august = Expense.where('extract(month from created_at) = ?', '10')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '10')
     @total = @august.pluck(:amount).sum
     average_a
     pack
   end
 
   def nov
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '11')
-    @august = Expense.where('extract(month from created_at) = ?', '11')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '11')
     @total = @august.pluck(:amount).sum
     average_b
     pack
   end
 
   def dec
-    # @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '12')
-    @august = Expense.where('extract(month from created_at) = ?', '12')
+    @august = Expense.where("cast(strftime('%m', created_at) as int) = ?", '12')
     @total = @august.pluck(:amount).sum
     average_a
     pack
