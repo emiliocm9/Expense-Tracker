36ce780b6bf7501ccaba64e97d2c8a9352e81bd5
emiliocm9 <emilio.contreras97@gmail.com>
2020-09-08 10:47:16 -0500

    docs: helper method for expenses show view

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -14,4 +14,16 @@ module ExpensesHelper
       image_tag 'https://bulma.io/images/placeholders/128x128.png', class: 'w-80'
     end
   end
+
+  def expense_id(expense)
+    code = ''
+    return unless expense.group_id
+
+    code << '<div class="total_big-container d-flex align-items-center pt-3 pb-3 bg-light justify-content-between">'
+    code << '<div class="total_title-container"><p class="text-casual-bold text-size text-size-small">Group</p>'
+    code << '</div><div class="total_title-container"><p class="text-casual-bold text-size-small">'
+    code << expense.group.name
+    code << '</p></div></div>'
+    code.html_safe
+  end
 end
